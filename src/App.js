eed7b3b2de9dfeaa83bf871dfa532de9a6982ae8
Lihini Galagedara <navo.galagedara@gmail.com>
2021-04-27 08:12:54 +0530

    #001# -m axios.get method implemented successfully. datafetch and assigning to fields successful.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import User from "./userComponent/User";
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state={
+            id :'',
+            email:'',
+            first_name:'',
+            last_name:'',
+            avatar:''
+        }
+
+    }
+
+    componentDidMount(){
+
+        axios.get('https://reqres.in/api/users/2').then((res)=>{
+            const id = res.data.id;
+            this.setState({id});
+            console.log(res.data.data.id);
+            const email = res.data.email;
+            console.log(res.data.data.email);
+            this.setState({email});
+            const first_name= res.data.first_name;
+            this.setState({first_name});
+            const last_name=res.data.last_name;
+            this.setState({last_name});
+            const avatar=res.data.avatar;
+            this.setState({avatar});
+
+
+        })
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <User/>
+            </div>
+        );
+    }
 }
 
 export default App;
